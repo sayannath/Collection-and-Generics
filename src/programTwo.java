2f12a2876b9c3cf1d4298a29e5826516dd9a0abc
sayannath <sayannath235@gmail.com>
2020-03-18 02:58:34 +0530

    Program Two

---
+++ src/programTwo.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class programTwo {
+    public static void main(String args[])throws IOException {
+
+        int n = 5;
+
+        ArrayList<Integer> arrayList = new ArrayList<>(n);
+
+        //Adding the elements in the collections.
+        for(int i = 1; i<n; i++)
+            arrayList.add(i);
+
+        //Before Sorting.
+        System.out.print(arrayList);
+        System.out.println();
+
+        //Sorting in ascending Order.
+        Collections.sort(arrayList);
+        System.out.print(arrayList);
+        System.out.println();
+
+
+        //Sorting in Descending Order.
+        Collections.sort(arrayList, Collections.reverseOrder());
+        System.out.print(arrayList);
+        System.out.println();
+
+
+    }
+}
