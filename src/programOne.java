8a9608206d2517906840be5eace82f11ad983b7c
sayannath <sayannath235@gmail.com>
2020-03-18 02:23:28 +0530

    Program One

---
+++ src/programOne.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class programOne {
+    public static void main(String args[])throws IOException {
+        int n = 5;
+        ArrayList<Integer> arrayList = new ArrayList<>(n);
+
+        for (int i=1; i <= n;i++)
+            arrayList.add(i);
+
+        for (int i =0; i<n;i++)
+            System.out.print(arrayList.get(i)+" ");
+    }
+}
