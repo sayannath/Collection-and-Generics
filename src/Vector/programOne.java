3985801b786430dccb0310bd99ccce6f53c29a7c
sayannath <sayannath235@gmail.com>
2020-04-08 22:10:06 +0530

    Add

--- src/Vector/programOne.java
+++ src/Vector/programOne.java
@@ -1,4 +1,20 @@
 package Vector;
 
+import java.util.Vector;
+
 public class programOne {
+    public static void main(String args[]) {
+
+        Vector<Integer> v = new Vector<>();
+
+        v.add(10);
+        v.add(20);
+        v.add(30);
+        v.add(40);
+        v.add(50);
+
+        System.out.println(v);
+        System.out.println(v.getClass().getName());
+
+    }
 }
